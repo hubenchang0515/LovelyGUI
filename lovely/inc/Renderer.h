aaab7b6662f0c668b02ce6badf117dc232a4f65f
hubenchang0515 <hubenchang0515@outlook.com>
2019-08-07 20:04:14 +0800

    warp class Renderer

---
+++ lovely/inc/Renderer.h
@@ -0,0 +1,73 @@
+#ifndef LOVELY_GUI_RENDERER
+#define LOVELY_GUI_RENDERER
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <vector>
+
+#include "Window.h"
+
+namespace LovelyGUI
+{
+
+using Point = SDL_Point;
+using Rect = SDL_Rect;
+using Color = SDL_Color;
+using Texture = SDL_Texture;
+using Event = SDL_Event;
+
+enum class BlendMode
+{
+    None = SDL_BLENDMODE_NONE,
+    Blend = SDL_BLENDMODE_BLEND,
+    Add = SDL_BLENDMODE_ADD,
+    Mod = SDL_BLENDMODE_MOD,
+};
+
+class Renderer
+{
+public:
+    Renderer(Window* _window);
+    ~Renderer();
+
+    /* Thread Sync */
+    int lock();
+    int unlock();
+    int tryLock();
+
+    /* Texture */
+    Texture* createTexture(Uint32 width, Uint32 height);
+    void destroyTexture(Texture* texture);
+
+    /* Setting */
+    int setTarget(Texture* texture=nullptr);
+    int setBlendMode(BlendMode mode=BlendMode::None);
+    int setColor(Uint8 r, Uint8 g, Uint8 b);
+    int setColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+    int setColor(const Color& color);
+
+    /* Draw */
+    int clear();
+    void present();
+    int drawPoint(int x, int y);
+    int drawPoint(const Point& point);
+    int drawPoints(const std::vector<Point>& points);
+    int drawLine(int x1, int y1, int x2, int y2);
+    int drawLine(const Point& point1, const Point& point2);
+    int drawLines(const std::vector<Point>& points);
+    int drawRect(const Rect& rect);
+    int drawRects(const std::vector<Rect>& rects);
+    int fillRect(const Rect& rect);
+    int fillRects(const std::vector<Rect>& rects);
+    int copy(Texture* source, const Rect* dstRect, const Rect* srcRect);
+
+private:
+    SDL_Renderer* _renderer;
+    SDL_mutex* _mutex;
+};
+
+}; // namespace LovelyGUI
+
+#endif
---
+++ lovely/src/Renderer.cpp
@@ -0,0 +1,178 @@
+#include "Renderer.h"
+#include <stdexcept>
+
+namespace LovelyGUI
+{
+
+Renderer::Renderer(Window* window)
+{
+    SDL_Window* sdlWindow = window->window();
+
+    /* Create Renderer */
+    this->_renderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_ACCELERATED);
+    if(this->_renderer == nullptr)
+    {
+        this->_renderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_SOFTWARE);
+        if(this->_renderer == nullptr)
+        {
+            SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateRenderer : %s", SDL_GetError());
+            throw std::runtime_error("SDL_CreateRenderer Error");
+        }
+    }
+
+    /* Create mutex */
+    this->_mutex = SDL_CreateMutex();
+    if(this->_mutex == nullptr)
+    {
+        SDL_DestroyRenderer(this->_renderer);
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateMutex : %s", SDL_GetError());
+        throw std::runtime_error("SDL_CreateMutex Error");
+    }
+}
+
+Renderer::~Renderer()
+{
+    if(this->_mutex != nullptr)
+    {
+        SDL_UnlockMutex(this->_mutex);
+    }
+    SDL_DestroyMutex(this->_mutex);
+    SDL_DestroyRenderer(this->_renderer);
+}
+
+
+int Renderer::lock()
+{
+    return SDL_LockMutex(this->_mutex);
+}
+
+
+int Renderer::unlock()
+{
+    return SDL_UnlockMutex(this->_mutex);
+}
+
+
+int Renderer::tryLock()
+{
+    return SDL_TryLockMutex(this->_mutex);
+}
+
+
+Texture* Renderer::createTexture(Uint32 width, Uint32 height)
+{
+    return SDL_CreateTexture(this->_renderer, SDL_PIXELFORMAT_RGBA8888, 
+                                SDL_TEXTUREACCESS_TARGET, width, height);
+}
+
+
+void Renderer::destroyTexture(Texture* texture)
+{
+    SDL_DestroyTexture(texture);
+}
+
+
+int Renderer::setTarget(Texture* texture)
+{
+    return SDL_SetRenderTarget(this->_renderer, texture);
+}
+
+
+int Renderer::setBlendMode(BlendMode mode)
+{
+    return SDL_SetRenderDrawBlendMode(this->_renderer, static_cast<SDL_BlendMode>(mode));
+}
+
+
+int Renderer::setColor(Uint8 r, Uint8 g, Uint8 b)
+{
+    return SDL_SetRenderDrawColor(this->_renderer, r, g, b, 0);
+}
+
+
+int Renderer::setColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+    return SDL_SetRenderDrawColor(this->_renderer, r, g, b, a);
+}
+
+
+int Renderer::setColor(const Color& color)
+{
+    return SDL_SetRenderDrawColor(this->_renderer, color.r, color.g, color.b, color.a);
+}
+
+
+int Renderer::clear()
+{
+    return SDL_RenderClear(this->_renderer);
+}
+
+
+void Renderer::present()
+{
+    SDL_RenderPresent(this->_renderer);
+}
+
+
+int Renderer::drawPoint(int x, int y)
+{
+    return SDL_RenderDrawPoint(this->_renderer, x, y);
+}
+
+
+int Renderer::drawPoint(const Point& point)
+{
+    return SDL_RenderDrawPoint(this->_renderer, point.x, point.y);
+}
+
+
+int Renderer::drawPoints(const std::vector<Point>& points)
+{
+    return SDL_RenderDrawPoints(this->_renderer, points.data(), points.size());
+}
+
+
+int Renderer::drawLine(int x1, int y1, int x2, int y2)
+{
+    return SDL_RenderDrawLine(this->_renderer, x1, y1, x2, y2);
+}
+
+
+int Renderer::drawLine(const Point& point1, const Point& point2)
+{
+    return SDL_RenderDrawLine(this->_renderer, point1.x, point1.y, point2.x, point2.y);
+}
+
+
+int Renderer::drawLines(const std::vector<Point>& points)
+{
+    return SDL_RenderDrawLines(this->_renderer, points.data(), points.size());
+}
+
+
+int Renderer::drawRect(const Rect& rect)
+{
+    return SDL_RenderDrawRect(this->_renderer, &rect);
+}
+
+int Renderer::drawRects(const std::vector<Rect>& rects)
+{
+    return SDL_RenderDrawRects(this->_renderer, rects.data(), rects.size());
+}
+
+int Renderer::fillRect(const Rect& rect)
+{
+    return SDL_RenderFillRect(this->_renderer, &rect);
+}
+
+int Renderer::fillRects(const std::vector<Rect>& rects)
+{
+    return SDL_RenderFillRects(this->_renderer, rects.data(), rects.size());
+}
+
+int Renderer::copy(Texture* source, const Rect* dstRect, const Rect* srcRect)
+{
+    return SDL_RenderCopy(this->_renderer, source, dstRect, srcRect);
+}
+
+}; // namespace LovelyGUI
